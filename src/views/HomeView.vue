c875dcb7dfad76fcb896210a4c8046cce37b7e58
Benjamin Schleiss <Benjamin_Schleiss@sluz.ch>
2022-06-02 14:45:30 +0200

    removed template stuff

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Hallo Welt!</h1>
+    <ul>
+      <li v-for="item in items">{{ item }}</li>
+    </ul>
+    <input @keyup.enter="addItem" v-model="newItem" />
   </div>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { ref } from "vue";
+import HelloWorld from "@/components/HelloWorld.vue";
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  setup: () => {
+    const newItem = ref("");
+    const items = ref(["Äpfel", "Birnen", "Banane"]);
+    function addItem() {
+      items.value.push(newItem.value);
+    }
+    return { newItem, items, addItem };
+  },
+  components: { HelloWorld },
+};
 </script>
+<style>
+* {
+  color: red;
+}
+</style>
